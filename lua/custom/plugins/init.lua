aea5be42ecd772a1792d72be9c4a60c3f879d76b
AS <asims178@gmail.com>
2024-03-26 08:06:20 -0400

    fix: add lazygit plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -36,4 +36,23 @@ return {
       url = "http://ubuntu.local:11434",
     }
   },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
 }

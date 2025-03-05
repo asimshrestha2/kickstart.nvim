256bd72892f8a8cb1c6fd5b800bc4cdb6da8429a
asimshrestha2 <asim178@gmail.com>
2025-03-05 15:15:54 -0500

    add gdscript

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+require'lspconfig'.gdscript.setup{}
+
 return {
   {
     "nomnivore/ollama.nvim",

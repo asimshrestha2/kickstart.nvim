484a1adb50a8bff417532e16cd19031e413a918d
Asim <asimshrestha@proton.me>
2025-05-22 10:07:53 -0400

    feat: notify

--- init.lua
+++ init.lua
@@ -81,6 +81,8 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 
+vim.opt.termguicolors = true
+
 vim.o.textwidth = 120
 vim.o.colorcolumn = '-2'
 
---
+++ lua/custom/plugins/notify.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'rcarriga/nvim-notify',
+    -- @type nofiy.Config
+    opt = {
+      background_colour = '#000000',
+    },
+    config = function()
+      vim.cmd [[
+		hi NotifyBackground guibg = #000000
+	]]
+      vim.notify = require 'notify'
+    end,
+  },
+}

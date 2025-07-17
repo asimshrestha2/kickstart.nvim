1fac5e5265cedd9c0cadfd21fa50f209f741d3d3
AS <asims178@gmail.com>
2025-07-16 21:05:38 -0400

    add image

---
+++ lua/custom/plugins/image.lua
@@ -0,0 +1,26 @@
+return {
+  '3rd/image.nvim',
+  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+  opts = {
+    processor = 'magick_cli',
+    backend = 'kitty',
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = false,
+        filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
+      },
+      neorg = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = false,
+        filetypes = { 'norg' },
+      },
+    },
+    max_height_window_percentage = 50,
+    kitty_method = 'normal',
+  },
+}
